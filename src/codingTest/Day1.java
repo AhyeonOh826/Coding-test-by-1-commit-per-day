f55c826bd14e8413e3f35606593c8e194a49acfb
ooo90 <ahyeonOh826@gmail.com>
2021-04-15 22:02:25 +0900

    test

--- src/codingTest/Day1.java
+++ src/codingTest/Day1.java
@@ -5,6 +5,10 @@ public class Day1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("출력 확인1");
+		System.out.println("출력 확인1");
+		System.out.println("출력 확인1");
+		System.out.println("출력 확인1");
+		System.out.println("출력 확인1");
 	}
 
 }
