8b27a2b777c6fe46403d9f6c0af33bcfcf8b10cd
ooo90 <ahyeonOh826@gmail.com>
2021-04-15 21:57:02 +0900

    commit test

--- src/codingTest/Day1.java
+++ src/codingTest/Day1.java
@@ -1,10 +1,10 @@
 package codingTest;
 
-public class day1 {
+public class Day1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("출력 확인");
 	}
 
 }
